7203f68df02957f8384e5bcdd766bb84ae739e43
Winner Isac KODJO <kodjowinnerisac@gmail.com>
2025-04-20 11:44:37 +0200

    Modification du fichier cv.route.js

--- backend/src/routes/cv.routes.js
+++ backend/src/routes/cv.routes.js
@@ -3,10 +3,17 @@ const { saveCV, getUserCVs } = require('../controllers/cv.controller');
 const { protect } = require('../middlewares/auth.middleware');
 const router = express.Router();
 
+// // Route to save a CV
+// router.post('/', protect, saveCV);
+
+// // Route to get user's CVs
+// router.get('/', protect, getUserCVs);
+
+
 // Route to save a CV
-router.post('/', protect, saveCV);
+router.post('/', saveCV);
 
 // Route to get user's CVs
-router.get('/', protect, getUserCVs);
+router.get('/', getUserCVs);
 
 module.exports = router;
